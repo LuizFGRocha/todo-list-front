25913e5fe5f5f03529da7f4de831e0aec05062d4
Luiz Fernando <luizfgrocha04@gmail.com>
2024-05-26 18:23:08 -0300

    Criada error boundary

---
+++ src/components/error/ErrorBoundary.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSessionContext } from '../../auth/authProvider';
+
+class ErrorBoundary extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    const nav = this.props.nav;
+    const logout = this.props.logout;
+    if (this.state.hasError) {
+      logout();
+      nav('/login');
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
