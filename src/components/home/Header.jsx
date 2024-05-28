71e44aaf4c9c8a0fb997b2e69fcf6114f8f112a8
Luiz Fernando <luizfgrocha04@gmail.com>
2024-05-28 03:44:35 -0300

    Adicionado comentário

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -6,10 +6,13 @@ const Header = () => {
     <nav className="bg-blue-300 shadow dark:bg-gray-800 flex flex-row">
       <div className="flex items-center justify-between w-full p-4 mx-auto text-gray-600 dark:text-gray-300">
         <div className="flex items-center  mx-1.5 sm:mx-6 gap-1">
+
+          {/* Logo no header */}
           <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" className="bi bi-check2-square fill-gray-800 dark:fill-gray-200" viewBox="0 0 16 16" style={{paddingTop: "2px"}}>
             <path d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5z"/>
             <path d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0"/>
           </svg>
+
           <h1
             className="text-gray-800 text-2xl text-bold transition-colors duration-300 transform dark:text-gray-200"
           >
